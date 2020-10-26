9be3e758835c9e8ec2dd9ece6f3d6ff9abde42e3
Brayssen <btorrejoncerna@gmail.com>
2020-10-26 17:45:23 -0600

    Version 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react';
+import {BrowserRouter, Switch,Route} from "react-router-dom";
+import Axios from "axios";
+import Header from "./components/Layout/Header";
+import Home from "./components/pages/Home";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import UserContext from "./context/UserContext";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./style.css";
+
+export default function App() {
+    const [userData, setUserData] = useState({
+        token: undefined,
+        user: undefined,
+    });//Validado
+
+    useEffect(() => {
+        const checkLoggedIn = async () => {
+            let token = localStorage.getItem("auth-token");
+            if(token === null){
+                localStorage.setItem("auth-token","");
+                token = "";
+            }
+            const tokenRes = await Axios.post("http://localhost:5000/users/tokenIsValid",null,{headers:{"x-auth-token": token}}
+            );
+            if(tokenRes.data){
+                const userRes = await Axios.get("http://localhost:5000/users/",{headers:{"x-auth-token": token},
+            });
+                setUserData({
+                    token,
+                    user: userRes.data,
+                });
+                //console.log(token);
+            }
+            
+        };
+        checkLoggedIn();
+    },[]);
+    return (
+        <>
+            <BrowserRouter>
+                <UserContext.Provider value={{userData, setUserData}}>
+                    <Header/>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route exact path="/Login" component={Login}/>
+                        <Route exact path="/Register" component={Register}/>
+                    </Switch>
+                </UserContext.Provider>
+            </BrowserRouter>
+        </>
+    )
 }
-
-export default App;
